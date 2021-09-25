7784f19563048d8025e47867b3a507fef325ea65
julian Amacker <julian.amacker@protonmail.ch>
2021-09-25 17:23:01 +0200

    remove num floatings

--- src/math/num.rs
+++ src/math/num.rs
@@ -12,7 +12,7 @@ use rust_decimal::{Decimal}; //to delete
 use rust_decimal::prelude::*;
 use num::*;
 use std::cmp::{PartialOrd, PartialEq};
-use smartcore::math::num::RealNumber;
+//use smartcore::math::num::RealNumber;
 
 /// Defines real number
 /// <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML"></script>
